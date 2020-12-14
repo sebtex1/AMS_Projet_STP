d7efb67ca5406fbea86f7212ec1db9dceb452735
SebTex <36498890+sebtex1@users.noreply.github.com>
2020-12-14 09:42:37 +0100

    Add category

---
+++ category/src/test/java/stp/projet/category/CategoryApplicationTests.java
@@ -0,0 +1,13 @@
+package stp.projet.category;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
