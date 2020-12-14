1ab083dc0a1685ad6c75dc10e62df8e750604595
SebTex <36498890+sebtex1@users.noreply.github.com>
2020-12-14 15:03:46 +0100

    Add basic webclient

---
+++ web-Client/src/main/java/stp/projet/webClient/controller/WebClientController.java
@@ -0,0 +1,15 @@
+package stp.projet.webClient.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebClientController {
+
+	
+	@GetMapping("/")
+	public String getHomePage(Model model) {			
+		return "homePage";
+	}
+}
